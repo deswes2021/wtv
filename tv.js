a1820c7951cf47ef0744079edc9d65f834571bad
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2024-12-17 20:59:43 -0600

    Update tv.js

--- tv.js
+++ tv.js
@@ -1,3 +1,32 @@
 [
-  
+    {
+        grupo: 'tv',
+        image: '/img/tv/nacional.jpg',
+        local: 'nacional',
+    },
+    {
+        grupo: 'tv',
+        image: '/img/tv/infantil.jpg',
+        local: 'infantil',
+    },
+    {
+        grupo: 'tv',
+        image: '/img/tv/variado.jpg',
+        local: 'variado',
+    },
+    {
+        grupo: 'tv',
+        image: '/img/tv/deporte.jpg',
+        local: 'deporte',
+    },
+    {
+        grupo: 'tv',
+        image: '/img/tv/247.jpg',
+        local: '247',
+    },
+    {
+        grupo: 'tv',
+        image: '/img/tv/premium.jpg',
+        local: 'premium',
+    }
 ]
