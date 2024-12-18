0bc474ac3d8db11ef9775e3422568e9a6d7a32b1
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2024-12-17 21:18:07 -0600

    Update opciones.js

--- opciones.js
+++ opciones.js
@@ -1,1 +1,12 @@
-[]
+[
+  {
+    grupo: 'opciones',
+    image: '/img/opciones/activar.jpg',
+    local: 'nacional'
+  },
+  {
+    grupo: 'opciones',
+    image: '/img/opciones/actualizar.jpg',
+    local: 'nacional'
+  }
+]
