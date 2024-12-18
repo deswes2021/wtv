2a0ca439dc55ec72c93b107133d2f7164696ff04
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2024-12-17 20:08:33 -0600

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+[
+    {
+        grupo: 'menu',
+        image: './tv/tv.jpg',
+        local: 'tv',
+    },
+    {
+        grupo: 'menu',
+        image: './opciones/opciones.jpg',
+        local: 'opciones',
+    },
+    {
+        grupo: 'menu',
+        image: './cine/cine.jpg',
+        local: 'cine',
+    }
+]
