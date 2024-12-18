cb288d54e20e6fc00351513232d6c64355ef819a
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2024-12-17 21:32:27 -0600

    Update nacional.js

--- nacional.js
+++ nacional.js
@@ -2,22 +2,22 @@
   {
     grupo: 'nacional',
     image: '/img/nacional/canal1.jpg',
-    local: ''
+    local: 'https://telecorporacion.cdn.vustreams.com/live/19b307cf-3f2d-44cb-bce6-0fd65365c56a/live.isml/live.m3u8'
   },
   {
     grupo: 'nacional',
     image: '/img/nacional/canal2.jpg',
-    local: ''
+    local: 'https://telecorporacion-es.cdn.vustreams.com/live/d3e259fa-736d-46b0-b1c9-71caf946ace9/live.isml/d3e259fa-736d-46b0-b1c9-71caf946ace9.m3u8'
   },
   {
     grupo: 'nacional',
     image: '/img/nacional/canal3.jpg',
-    local: ''
+    local: 'https://cloud2.streaminglivehd.com:1936/8048/8048/playlist.m3u8'
   },
   {
     grupo: 'nacional',
     image: '/img/nacional/canal4.jpg',
-    local: ''
+    local: 'https://telecorporacion-es.cdn.vustreams.com/live/5d245474-cad2-47bc-b605-ad04c619247d/live.isml/.m3u8'
   },
   {
     grupo: 'nacional',
@@ -27,7 +27,7 @@
   {
     grupo: 'nacional',
     image: '/img/nacional/canal6.jpg',
-    local: ''
+    local: 'https://telecorporacion.cdn.vustreams.com/live/b164ebe7-decf-4a5a-8aea-5bb56fb92dfc/live.isml/live.m3u8'
   },
   {
     grupo: 'nacional',
@@ -42,7 +42,7 @@
   {
     grupo: 'nacional',
     image: '/img/nacional/canal9.jpg',
-    local: ''
+    local: 'https://streaming.asamblea.gob.sv/hls/plenariahd.m3u8'
   },
   {
     grupo: 'nacional',
@@ -57,7 +57,7 @@
   {
     grupo: 'nacional',
     image: '/img/nacional/canal12.jpg',
-    local: ''
+    local: 'https://alba-sv-c12-c12.stream.mediatiquestream.com/index.m3u8'
   },
   {
     grupo: 'nacional',
@@ -72,12 +72,12 @@
   {
     grupo: 'nacional',
     image: '/img/nacional/canal19.jpg',
-    local: ''
+    local: 'https://mgv-channel19.akamaized.net/hls/live/2093190/MGV_CHANNEL19/master.m3u8'
   },
   {
     grupo: 'nacional',
     image: '/img/nacional/canal21.jpg',
-    local: ''
+    local: 'https://mgv-channel21.akamaized.net/hls/live/2093191/MGV_CHANNEL21/master.m3u8'
   },
   {
     grupo: 'nacional',
