df216e3ebc7b54b2db106716e6b6494b0c924661
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2024-12-17 21:27:24 -0600

    Update and rename nacional to nacional.js

---
+++ nacional.js
@@ -0,0 +1,117 @@
+[
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal1.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal2.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal3.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal4.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal5.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal6.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal7.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal8.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal9.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal10.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal11.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal12.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal15.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal17.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal19.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal21.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal23.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal25.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal27.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal28.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal33.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal61.jpg',
+    local: ''
+  },
+  {
+    grupo: 'nacional',
+    image: '/img/nacional/canal65.jpg',
+    local: ''
+  }
+]
