f820fb48b31195723f3432d04de392e0d027a045
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2024-12-21 17:14:00 -0600

    Create list.js

---
+++ list.js
@@ -0,0 +1,42 @@
+[
+  {
+    gpo: "menu",
+    img: "",
+    loc: "nacional"
+  },
+  {
+    gpo: "menu",
+    img: "",
+    loc: "intanfil"
+  },
+  {
+    gpo: "menu",
+    img: "",
+    loc: "variado"
+  },
+  {
+    gpo: "menu",
+    img: "",
+    loc: "premium"
+  },
+  {
+    gpo: "menu",
+    img: "",
+    loc: "opciones"
+  },
+  {
+    gpo: "menu",
+    img: "",
+    loc: "deportes"
+  },
+  {
+    gpo: "menu",
+    img: "",
+    loc: "247"
+  },
+  {
+    gpo: "menu",
+    img: "",
+    loc: "cine"
+  }
+]
