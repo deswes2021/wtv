525df8d790ba1af8306d0480cc85dfb69a6e52e5
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2024-12-21 20:14:36 -0600

    Update list.js

--- list.js
+++ list.js
@@ -43,5 +43,83 @@
     gpo: "menu",
     img: "youtube.jpg",
     loc: "youtube"
-  }
+  },
+  /*----------------------------------------------------------*/
+  /*---nacional-----------------------------------------------*/
+  /*----------------------------------------------------------*/
+  {
+    gpo: "nacional",
+    img: "/nacional/canal1.jpg",
+    loc: "https://telecorporacion.cdn.vustreams.com/live/19b307cf-3f2d-44cb-bce6-0fd65365c56a/live.isml/live.m3u8"
+  },
+  {
+    gpo: "nacional",
+    img: "/nacional/canal2.jpg",
+    loc: "https://telecorporacion-es.cdn.vustreams.com/live/d3e259fa-736d-46b0-b1c9-71caf946ace9/live.isml/d3e259fa-736d-46b0-b1c9-71caf946ace9.m3u8"
+  },
+  {
+    gpo: "nacional",
+    img: "/nacional/canal3.jpg",
+    loc: "https://cloud2.streaminglivehd.com:1936/8048/8048/playlist.m3u8"
+  },
+  {
+    gpo: "nacional",
+    img: "/nacional/canal4.jpg",
+    loc: "https://cloud2.streaminglivehd.com:1936/8048/8048/playlist.m3u8"
+  },
+  {
+    gpo: "nacional",
+    img: "/nacional/canal5.jpg",
+    loc: ""
+  },
+  {
+    gpo: "nacional",
+    img: "/nacional/canal6.jpg",
+    loc: "https://telecorporacion.cdn.vustreams.com/live/b164ebe7-decf-4a5a-8aea-5bb56fb92dfc/live.isml/live.m3u8"
+  },
+  {
+    gpo: "nacional",
+    img: "/nacional/canal7.jpg",
+    loc: ""
+  },
+  {
+    gpo: "nacional",
+    img: "/nacional/canal8.jpg",
+    loc: ""
+  },
+  {
+    gpo: "nacional",
+    img: "/nacional/canal9.jpg",
+    loc: "https://streaming.asamblea.gob.sv/hls/plenariahd.m3u8"
+  },
+  {
+    gpo: "nacional",
+    img: "/nacional/canal10.jpg",
+    loc: ""
+  },
+  {
+    gpo: "nacional",
+    img: "/nacional/canal11.jpg",
+    loc: ""
+  },
+  {
+    gpo: "nacional",
+    img: "/nacional/canal12.jpg",
+    loc: "https://alba-sv-c12-c12.stream.mediatiquestream.com/index.m3u8"
+  },
+  {
+    gpo: "nacional",
+    img: "/nacional/canal15.jpg",
+    loc: ""
+  },
+  {
+    gpo: "nacional",
+    img: "/nacional/canal19.jpg",
+    loc: "https://mgv-channel19.akamaized.net/hls/live/2093190/MGV_CHANNEL19/master.m3u8"
+  },
+  {
+    gpo: "nacional",
+    img: "/nacional/cana21.jpg",
+    loc: "https://mgv-channel21.akamaized.net/hls/live/2093191/MGV_CHANNEL21/master.m3u8"
+  },
 ]
