9174022187f894f4735cf863be5a21959121985f
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2024-12-29 23:30:12 -0600

    Update list.js

--- list.js
+++ list.js
@@ -1,138 +1,138 @@
-[
+var nknl = [
   {
-    gpo: "menu",
-    img: "nacional.jpg",
-    loc: "nacional"
+    tipo: "menu",
+    logo: "nacional.jpg",
+    url: "nacional"
   },
   {
-    gpo: "menu",
-    img: "infantil.jpg",
-    loc: "infantil"
+    tipo: "menu",
+    logo: "infantil.jpg",
+    url: "infantil"
   },
   {
-    gpo: "menu",
-    img: "variado.jpg",
-    loc: "variado"
+    tipo: "menu",
+    logo: "variado.jpg",
+    url: "variado"
   },
   {
-    gpo: "menu",
-    img: "premium.jpg",
-    loc: "premium"
+    tipo: "menu",
+    logo: "premium.jpg",
+    url: "premium"
   },
   {
-    gpo: "menu",
-    img: "opcion.jpg",
-    loc: "opcion"
+    tipo: "menu",
+    logo: "opcion.jpg",
+    url: "opcion"
   },
   {
-    gpo: "menu",
-    img: "deporte.jpg",
-    loc: "deporte"
+    tipo: "menu",
+    logo: "deporte.jpg",
+    url: "deporte"
   },
   {
-    gpo: "menu",
-    img: "247.jpg",
-    loc: "247"
+    tipo: "menu",
+    logo: "247.jpg",
+    url: "247"
   },
   {
-    gpo: "menu",
-    img: "cinema.jpg",
-    loc: "cinema"
+    tipo: "menu",
+    logo: "cinema.jpg",
+    url: "cinema"
   },
   {
-    gpo: "menu",
-    img: "youtube.jpg",
-    loc: "youtube"
+    tipo: "menu",
+    logo: "youtube.jpg",
+    url: "youtube"
   },
   /*----------------------------------------------------------*/
   /*---opciones-----------------------------------------------*/
   /*----------------------------------------------------------*/
   {
-    gpo: "opcion",
-    img: "/opcion/activar.jpg",
-    loc: "https://teleclub.xyz/activar"
+    tipo: "opcion",
+    logo: "/opcion/activar.jpg",
+    url: "https://teleclub.xyz/activar"
   },
   {
-    gpo: "opcion",
-    img: "/opcion/actualizar.jpg",
-    loc: "actualizar"
+    tipo: "opcion",
+    logo: "/opcion/actualizar.jpg",
+    url: "actualizar"
   },
   /*----------------------------------------------------------*/
   /*---nacional-----------------------------------------------*/
   /*----------------------------------------------------------*/
   {
-    gpo: "nacional",
-    img: "/nacional/canal1.jpg",
-    loc: "https://telecorporacion.cdn.vustreams.com/live/19b307cf-3f2d-44cb-bce6-0fd65365c56a/live.isml/live.m3u8"
+    tipo: "nacional",
+    logo: "/nacional/canal1.jpg",
+    url: "https://telecorporacion.cdn.vustreams.com/live/19b307cf-3f2d-44cb-bce6-0fd65365c56a/live.isml/live.m3u8"
   },
   {
-    gpo: "nacional",
-    img: "/nacional/canal2.jpg",
-    loc: "https://telecorporacion-es.cdn.vustreams.com/live/d3e259fa-736d-46b0-b1c9-71caf946ace9/live.isml/d3e259fa-736d-46b0-b1c9-71caf946ace9.m3u8"
+    tipo: "nacional",
+    logo: "/nacional/canal2.jpg",
+    url: "https://telecorporacion-es.cdn.vustreams.com/live/d3e259fa-736d-46b0-b1c9-71caf946ace9/live.isml/d3e259fa-736d-46b0-b1c9-71caf946ace9.m3u8"
   },
   {
-    gpo: "nacional",
-    img: "/nacional/canal3.jpg",
-    loc: "https://cloud2.streaminglivehd.com:1936/8048/8048/playlist.m3u8"
+    tipo: "nacional",
+    logo: "/nacional/canal3.jpg",
+    url: "https://cloud2.streaminglivehd.com:1936/8048/8048/playlist.m3u8"
   },
   {
-    gpo: "nacional",
-    img: "/nacional/canal4.jpg",
-    loc: "https://cloud2.streaminglivehd.com:1936/8048/8048/playlist.m3u8"
+    tipo: "nacional",
+    logo: "/nacional/canal4.jpg",
+    url: "https://cloud2.streaminglivehd.com:1936/8048/8048/playlist.m3u8"
   },
   {
-    gpo: "nacional",
-    img: "/nacional/canal5.jpg",
-    loc: ""
+    tipo: "nacional",
+    logo: "/nacional/canal5.jpg",
+    url: ""
   },
   {
-    gpo: "nacional",
-    img: "/nacional/canal6.jpg",
-    loc: "https://telecorporacion.cdn.vustreams.com/live/b164ebe7-decf-4a5a-8aea-5bb56fb92dfc/live.isml/live.m3u8"
+    tipo: "nacional",
+    logo: "/nacional/canal6.jpg",
+    url: "https://telecorporacion.cdn.vustreams.com/live/b164ebe7-decf-4a5a-8aea-5bb56fb92dfc/live.isml/live.m3u8"
   },
   {
-    gpo: "nacional",
-    img: "/nacional/canal7.jpg",
-    loc: ""
+    tipo: "nacional",
+    logo: "/nacional/canal7.jpg",
+    url: ""
   },
   {
-    gpo: "nacional",
-    img: "/nacional/canal8.jpg",
-    loc: ""
+    tipo: "nacional",
+    logo: "/nacional/canal8.jpg",
+    url: ""
   },
   {
-    gpo: "nacional",
-    img: "/nacional/canal9.jpg",
-    loc: "https://streaming.asamblea.gob.sv/hls/plenariahd.m3u8"
+    tipo: "nacional",
+    logo: "/nacional/canal9.jpg",
+    url: "https://streaming.asamblea.gob.sv/hls/plenariahd.m3u8"
   },
   {
-    gpo: "nacional",
-    img: "/nacional/canal10.jpg",
-    loc: ""
+    tipo: "nacional",
+    logo: "/nacional/canal10.jpg",
+    url: ""
   },
   {
-    gpo: "nacional",
-    img: "/nacional/canal11.jpg",
-    loc: ""
+    tipo: "nacional",
+    logo: "/nacional/canal11.jpg",
+    url: ""
   },
   {
-    gpo: "nacional",
-    img: "/nacional/canal12.jpg",
-    loc: "https://alba-sv-c12-c12.stream.mediatiquestream.com/index.m3u8"
+    tipo: "nacional",
+    logo: "/nacional/canal12.jpg",
+    url: "https://alba-sv-c12-c12.stream.mediatiquestream.com/index.m3u8"
   },
   {
-    gpo: "nacional",
-    img: "/nacional/canal15.jpg",
-    loc: ""
+    tipo: "nacional",
+    logo: "/nacional/canal15.jpg",
+    url: ""
   },
   {
-    gpo: "nacional",
-    img: "/nacional/canal19.jpg",
-    loc: "https://mgv-channel19.akamaized.net/hls/live/2093190/MGV_CHANNEL19/master.m3u8"
+    tipo: "nacional",
+    logo: "/nacional/canal19.jpg",
+    url: "https://mgv-channel19.akamaized.net/hls/live/2093190/MGV_CHANNEL19/master.m3u8"
   },
   {
-    gpo: "nacional",
-    img: "/nacional/canal21.jpg",
-    loc: "https://mgv-channel21.akamaized.net/hls/live/2093191/MGV_CHANNEL21/master.m3u8"
+    tipo: "nacional",
+    logo: "/nacional/canal21.jpg",
+    url: "https://mgv-channel21.akamaized.net/hls/live/2093191/MGV_CHANNEL21/master.m3u8"
   },
 ]
