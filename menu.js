ed00a10d153acce95e3bcb4c4ab93fd9ab8bb0de
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2025-05-09 18:58:09 -0600

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,47 +1,17 @@
 [
   {
     tipo: "menu",
-    logo: "nacional.jpg",
-    url: "nacional"
+    logo: "herr.jpg",
+    url: "herr"
   },
   {
     tipo: "menu",
-    logo: "infantil.jpg",
-    url: "infantil"
-  },
-  {
-    tipo: "menu",
-    logo: "variado.jpg",
-    url: "variado"
+    logo: "esv.jpg",
+    url: "esv"
   },
   {
     tipo: "menu",
     logo: "premium.jpg",
     url: "premium"
   },
-  {
-    tipo: "menu",
-    logo: "opcion.jpg",
-    url: "opcion"
-  },
-  {
-    tipo: "menu",
-    logo: "deporte.jpg",
-    url: "deporte"
-  },
-  {
-    tipo: "menu",
-    logo: "247.jpg",
-    url: "247"
-  },
-  {
-    tipo: "menu",
-    logo: "cinema.jpg",
-    url: "cinema"
-  },
-  {
-    tipo: "menu",
-    logo: "youtube.jpg",
-    url: "youtube"
-  }
 ]
