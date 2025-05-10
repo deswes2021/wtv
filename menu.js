c95aa20e1c81a918736247a127f53426e1d1bbcb
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2025-05-09 18:49:52 -0600

    Update and rename list.js to menu.js

---
+++ menu.js
@@ -0,0 +1,47 @@
+[
+  {
+    tipo: "menu",
+    logo: "nacional.jpg",
+    url: "nacional"
+  },
+  {
+    tipo: "menu",
+    logo: "infantil.jpg",
+    url: "infantil"
+  },
+  {
+    tipo: "menu",
+    logo: "variado.jpg",
+    url: "variado"
+  },
+  {
+    tipo: "menu",
+    logo: "premium.jpg",
+    url: "premium"
+  },
+  {
+    tipo: "menu",
+    logo: "opcion.jpg",
+    url: "opcion"
+  },
+  {
+    tipo: "menu",
+    logo: "deporte.jpg",
+    url: "deporte"
+  },
+  {
+    tipo: "menu",
+    logo: "247.jpg",
+    url: "247"
+  },
+  {
+    tipo: "menu",
+    logo: "cinema.jpg",
+    url: "cinema"
+  },
+  {
+    tipo: "menu",
+    logo: "youtube.jpg",
+    url: "youtube"
+  }
+]
