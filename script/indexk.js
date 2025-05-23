9358bdb011489e40cc46e543241427e49bc40250
deswes2021 <89219703+deswes2021@users.noreply.github.com>
2025-05-23 14:06:15 -0600

    Add files via upload

---
+++ script/indexk.js
@@ -0,0 +1,219 @@
+[
+/*----------------------------------------------------------------------------------*/
+/*-OPCIONES-*/
+/*----------------------------------------------------------------------------------*/
+{
+  nombre: "Opciones",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/Opciones.png",
+  url:"Opciones"
+},
+{
+  nombre: "Nacionales",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/Nacionales.png",
+  url:"Nacionales"
+},
+{
+  nombre: "Infantiles",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/Infantiles.png",
+  url:"Infantiles"
+},
+{
+  nombre: "Variados",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/Variados.png",
+  url:"Variados"
+},
+{
+  nombre: "Cinemas",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/Cinemas.png",
+  url:"Cinemas"
+},
+{
+  nombre: "24x7",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/24x7.png",
+  url:"24x7"
+},
+{
+  nombre: "Deportes",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/Deportes.png",
+  url:"Deportes"
+},
+{
+  nombre: "Musica",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/Musica.png",
+  url:"Musica"
+},
+{
+  nombre: "YTBOeste",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/YTBOeste.png",
+  url:"YTBOeste"
+},
+{
+  nombre: "YTBWars",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/YTBWars.png",
+  url:"YTBWars"
+},
+{
+  nombre: "YTBDocumental",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/YTBDocumental.png",
+  url:"YTBDocumental"
+},  
+{
+  nombre: "YTBFantasy",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/YTBFantasy.png",
+  url:"YTBFantasy"
+},  
+{
+  nombre: "YTBFiction",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/YTBFiction.png",
+  url:"YTBFiction"
+},  
+{
+  nombre: "YTBHorror",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/YTBHorror.png",
+  url:"YTBHorror"
+},    
+{
+  nombre: "YTBAnime",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/YTBAnime.png",
+  url:"YTBAnime"
+},    
+{
+  nombre: "YTBFamily",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/YTBFamily.png",
+  url:"YTBFamily"
+},      
+{
+  nombre: "YTBDrama",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/YTBDrama.png",
+  url:"YTBDrama"
+},      
+{
+  nombre: "YTBMusical",
+  tipo:"Menu",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/YTBMusical.png",
+  url:"YTBMusical"
+},
+
+/*----------------------------------------------------------------------------------*/
+/*-OPCIONES-*/
+/*----------------------------------------------------------------------------------*/
+{
+  nombre: "Activar",
+  tipo:"Opciones",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/activar.png",
+  url:"https://tv.teleclub.xyz/activar"
+},
+{
+  nombre: "Actualizar",
+  tipo:"Opciones",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/actualizar.png",
+  url:"/actualizar"
+},
+/*----------------------------------------------------------------------------------*/
+/*-NACIONALES-*/
+/*----------------------------------------------------------------------------------*/
+{
+  nombre: "TCS +",
+  tipo:"Nacionales",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/tcs.png",
+  url:"https://telecorporacion.cdn.vustreams.com/live/19b307cf-3f2d-44cb-bce6-0fd65365c56a/live.isml/live.m3u8"
+},
+{
+  nombre: "Canal 2",
+  tipo:"Nacionales",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/canal2.png",
+  url:"https://telecorporacion-es.cdn.vustreams.com/live/d3e259fa-736d-46b0-b1c9-71caf946ace9/live.isml/d3e259fa-736d-46b0-b1c9-71caf946ace9.m3u8"
+},
+{
+  nombre: "Canal 3",
+  tipo:"Nacionales",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/canal3.png",
+  url:"https://cloud2.streaminglivehd.com:1936/8048/8048/playlist.m3u8"
+},
+{
+  nombre: "Canal 4",
+  tipo:"Nacionales",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/canal4.png",
+  url:"https://telecorporacion-es.cdn.vustreams.com/live/5d245474-cad2-47bc-b605-ad04c619247d/live.isml/live.m3u8"
+},
+{
+  nombre: "Canal 5",
+  tipo:"Nacionales",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/canal5.png",
+  url:""
+},
+{
+  nombre: "Canal 6",
+  tipo:"Nacionales",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/canal6.png",
+  url:"https://telecorporacion.cdn.vustreams.com/live/b164ebe7-decf-4a5a-8aea-5bb56fb92dfc/live.isml/live.m3u8"
+},
+{
+  nombre: "Canal 7",
+  tipo:"Nacionales",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/canal7.png",
+  url:"https://cdn.elsalvadordigital.com:1936/wowtv/smil:wowtv.smil/playlist.m3u8"
+},
+{
+  nombre: "Canal 8",
+  tipo:"Nacionales",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/canal8.png",
+  url:"http://201.247.102.189/tmp_hls/stream/index.m3u8"
+},
+{
+  nombre: "Canal 9",
+  tipo:"Nacionales",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/canal9.png",
+  url:"https://streaming.asamblea.gob.sv/hls/plenariahd.m3u8"
+},
+{
+  nombre: "Canal 10",
+  tipo:"Nacionales",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/canal10.png",
+  url:""
+},
+{
+  nombre: "Canal 11",
+  tipo:"Nacionales",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/canal11.png",
+  url:"https://stream.giostreaming.net:19360/canal11/canal11.m3u8"
+},
+{
+  nombre: "Canal 12",
+  tipo:"Nacionales",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/canal12.png",
+  url:"https://alba-sv-c12-c12.stream.mediatiquestream.com/index.m3u8"
+},
+{
+  nombre: "Canal 19",
+  tipo:"Nacionales",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/canal19.png",
+  url:"https://mgv-channel19.akamaized.net/hls/live/2093190/MGV_CHANNEL19/master.m3u8"
+},
+{
+  nombre: "Canal 21",
+  tipo:"Nacionales",
+  logo:"https://raw.githubusercontent.com/deswes2021/westv/main/impng/canal21.png",
+  url:"https://mgv-channel21.akamaized.net/hls/live/2093191/MGV_CHANNEL21/master.m3u8"
+}
+/*----------------------------------------------------------------------------------*/
+/*-TELECLUB-*/
+/*----------------------------------------------------------------------------------*/
+]
